320062a7cdf480a9ced7c0b971ed4e521e7f68b6
PersianBlue <71480723+PersianBlue@users.noreply.github.com>
2022-05-06 20:31:40 -0500

    added youtube video

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import * as React from 'react'
-import Layout from '../components/layout'
-import App from '../components/App'
-import ButtonMaster from '../components/ButtonMaster'
+import * as React from "react";
+import Layout from "../components/layout";
+import App from "../components/App";
+import ButtonMaster from "../components/ButtonMaster";
 import "../components/indexBackground.css";
 
 let counter = 1;
@@ -11,12 +11,21 @@ const IndexPage = () => {
     <Layout pageTitle="Home Page">
       <body>
         <p>Hey guys, welcome back to my Youtube channel!</p>
-        <App counter = {counter}/>
+        <App counter={counter} />
         <ButtonMaster />
+        <video width="320" height="240" controls>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/L57ox0iQU7A"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </video>
       </body>
-      
-      
     </Layout>
-  )
-}
-export default IndexPage
+  );
+};
+export default IndexPage;
